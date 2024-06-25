01f31f6f33820423c21b9433df835f8d09144232
SheEagle <2748719496@qq.com>
2024-06-25 20:11:54 +0800

    feat: update database.sql

--- database.sql
+++ database.sql
@@ -1,17 +1,17 @@
 /*
- Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
- Source Server         : 本地测试环境
+ Source Server         : company
  Source Server Type    : MySQL
- Source Server Version : 80034 (8.0.34)
+ Source Server Version : 50724
  Source Host           : localhost:3306
- Source Schema         : test
+ Source Schema         : study
 
  Target Server Type    : MySQL
- Target Server Version : 80034 (8.0.34)
+ Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 07/08/2023 00:03:19
+ Date: 25/06/2024 19:54:37
 */
 
 SET NAMES utf8mb4;
@@ -21,22 +21,175 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for db_account
 -- ----------------------------
 DROP TABLE IF EXISTS `db_account`;
-CREATE TABLE `db_account` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  `register_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_email` (`email`),
-  UNIQUE KEY `unique_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `db_account`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `role` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `register_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `unique_email`(`email`) USING BTREE,
+  UNIQUE INDEX `unique_username`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of db_account
+-- Table structure for db_account_details
 -- ----------------------------
-BEGIN;
-COMMIT;
+DROP TABLE IF EXISTS `db_account_details`;
+CREATE TABLE `db_account_details`  (
+  `id` int(11) NOT NULL,
+  `gender` tinyint(4) NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `qq` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `wx` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `introduction` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_account_privacy
+-- ----------------------------
+DROP TABLE IF EXISTS `db_account_privacy`;
+CREATE TABLE `db_account_privacy`  (
+  `id` int(11) NOT NULL,
+  `phone` tinyint(4) NULL DEFAULT NULL,
+  `email` tinyint(4) NULL DEFAULT NULL,
+  `qq` tinyint(4) NULL DEFAULT NULL,
+  `wx` tinyint(4) NULL DEFAULT NULL,
+  `gender` tinyint(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_chat
+-- ----------------------------
+DROP TABLE IF EXISTS `db_chat`;
+CREATE TABLE `db_chat`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `session_id` int(11) NOT NULL,
+  `question` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `answer` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id`) USING BTREE,
+  INDEX `db_session_id`(`session_id`) USING BTREE,
+  CONSTRAINT `db_chat_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `db_account` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `db_session_id` FOREIGN KEY (`session_id`) REFERENCES `db_chat_session` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 65 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_chat_session
+-- ----------------------------
+DROP TABLE IF EXISTS `db_chat_session`;
+CREATE TABLE `db_chat_session`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NULL DEFAULT NULL,
+  `session_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `created_at` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_image_store
+-- ----------------------------
+DROP TABLE IF EXISTS `db_image_store`;
+CREATE TABLE `db_image_store`  (
+  `uid` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `time` datetime NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_notification
+-- ----------------------------
+DROP TABLE IF EXISTS `db_notification`;
+CREATE TABLE `db_notification`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_topic
+-- ----------------------------
+DROP TABLE IF EXISTS `db_topic`;
+CREATE TABLE `db_topic`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `uid` int(11) NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `time` datetime NULL DEFAULT NULL,
+  `top` tinyint(4) NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 33 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_topic_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `db_topic_comment`;
+CREATE TABLE `db_topic_comment`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NULL DEFAULT NULL,
+  `tid` int(11) NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `time` datetime NULL DEFAULT NULL,
+  `quote` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 68 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_topic_interact_collect
+-- ----------------------------
+DROP TABLE IF EXISTS `db_topic_interact_collect`;
+CREATE TABLE `db_topic_interact_collect`  (
+  `tid` int(11) NULL DEFAULT NULL,
+  `uid` int(11) NULL DEFAULT NULL,
+  `time` datetime NULL DEFAULT NULL,
+  UNIQUE INDEX `tid_uid_collect`(`tid`, `uid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_topic_interact_like
+-- ----------------------------
+DROP TABLE IF EXISTS `db_topic_interact_like`;
+CREATE TABLE `db_topic_interact_like`  (
+  `tid` int(11) NULL DEFAULT NULL,
+  `uid` int(11) NULL DEFAULT NULL,
+  `time` datetime NULL DEFAULT NULL,
+  UNIQUE INDEX `tid_uid_like`(`tid`, `uid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for db_topic_type
+-- ----------------------------
+DROP TABLE IF EXISTS `db_topic_type`;
+CREATE TABLE `db_topic_type`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `color` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- View structure for user_post_statistics
+-- ----------------------------
+DROP VIEW IF EXISTS `user_post_statistics`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `user_post_statistics` AS select `db_account`.`id` AS `user_id`,`db_account`.`username` AS `user_name`,count(`db_topic_interact_like`.`tid`) AS `total_likes`,count(`db_topic_interact_collect`.`tid`) AS `total_collects`,(select count(0) from `db_topic_comment` where (`db_topic_comment`.`uid` = `db_account`.`id`)) AS `total_comments` from ((`db_account` left join `db_topic_interact_like` on((`db_topic_interact_like`.`uid` = `db_account`.`id`))) left join `db_topic_interact_collect` on((`db_topic_interact_collect`.`uid` = `db_account`.`id`))) group by `db_account`.`id`,`db_account`.`username`;
+
+-- ----------------------------
+-- View structure for view_topic_hotness
+-- ----------------------------
+DROP VIEW IF EXISTS `view_topic_hotness`;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `view_topic_hotness` AS select `t`.`id` AS `topic_id`,`t`.`title` AS `title`,`t`.`content` AS `content`,`t`.`uid` AS `uid`,`t`.`type` AS `type`,`t`.`time` AS `time`,coalesce(`likes`.`like_count`,0) AS `like_count`,coalesce(`collects`.`collect_count`,0) AS `collect_count`,coalesce(`comments`.`comment_count`,0) AS `comment_count`,(((coalesce(`likes`.`like_count`,0) * 2) + (coalesce(`collects`.`collect_count`,0) * 1.5)) + (coalesce(`comments`.`comment_count`,0) * 3)) AS `hotness` from (((`study`.`db_topic` `t` left join (select `study`.`db_topic_interact_like`.`tid` AS `tid`,count(0) AS `like_count` from `study`.`db_topic_interact_like` group by `study`.`db_topic_interact_like`.`tid`) `likes` on((`t`.`id` = `likes`.`tid`))) left join (select `study`.`db_topic_interact_collect`.`tid` AS `tid`,count(0) AS `collect_count` from `study`.`db_topic_interact_collect` group by `study`.`db_topic_interact_collect`.`tid`) `collects` on((`t`.`id` = `collects`.`tid`))) left join (select `study`.`db_topic_comment`.`tid` AS `tid`,count(0) AS `comment_count` from `study`.`db_topic_comment` group by `study`.`db_topic_comment`.`tid`) `comments` on((`t`.`id` = `comments`.`tid`)));
 
 SET FOREIGN_KEY_CHECKS = 1;
